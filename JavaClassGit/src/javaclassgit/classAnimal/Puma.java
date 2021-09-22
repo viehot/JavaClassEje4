40d809f76d24ed6a5d63d762de8aec5e1c903079
Nuria Romeu <nuriaromeu26@gmail.com>
2021-09-21 21:18:02 -0300

    agrego los atributos

---
+++ JavaClassGit/src/javaclassgit/classAnimal/Puma.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaclassgit.classAnimal;
+
+/**
+ *
+ * @author Nuria Romeu
+ */
+public class Puma {
+    private Integer velocidad;
+    private String nombre;
+    private String color;
+}
